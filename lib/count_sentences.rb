f774de19722e9f74d1a930a64ec216377520fcbd
Demi Conductor <demiconductor@outlook.com>
2017-04-07 12:12:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    text_array = self.split
+    text_array.each do |text|
+      if text.sentence? || text.question? || text.exclamation?
+        count+=1
+      end
+    end
+    return count
   end
 end
